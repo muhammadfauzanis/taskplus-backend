b1532164f09a202c39102c5505f4b292d9dd27a1
fikiaprian23 <fikiaprian23@gmail.com>
2023-12-26 12:31:08 +0700

    add getSubjectById & updateSubject

--- routes/subjectRoutes.js
+++ routes/subjectRoutes.js
@@ -4,8 +4,8 @@ import protectRoute from '../middlewares/protectRoute.js'
 
 const router = express.Router()
 
-router.get('/:subjectId', protectRoute, subjectController.getSubjectById)
 router.get('/list', protectRoute, subjectController.getSubjects)
+router.get('/:subjectId', protectRoute, subjectController.getSubjectById)
 router.get('/types', protectRoute, subjectController.getAllSubjectNames)
 router.put('/:subjectId', protectRoute, subjectController.updateSubject)
 router.post('/create', protectRoute, subjectController.createSubject)
